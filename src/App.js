87b1990bdb0e459f2cbce919df88e452d03b83cd
Rgaliant <ryanwfgale@gmail.com>
2019-05-08 15:36:50 -0600

    init react navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "react-bootstrap/Image";
+import logo from "./assets/IDLIFE_LOGO_BLACK2.png";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home" style={{ padding: "1.5rem" }}>
+          <Image src={logo} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Button
+          class="btn btn-primary"
+          data-check-referrer="true"
+          data-referrer-param="sponsor_id"
+          href="https://idlife.com/assessment?sponsor_id=&amp;referralid=&amp;Experience="
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Take Your FREE Assessment Now!
+        </Button>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <NavDropdown title="ALL PRODUCTS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#home">PEOPLE</Nav.Link>
+            <Nav.Link href="#link">SHOP</Nav.Link>
+            <Nav.Link style={{ paddingRight: "3rem" }} href="#link">
+              OPPORTUNITY
+            </Nav.Link>
+            <NavDropdown
+              style={{ borderRight: "1px solid #D3D3D3" }}
+              title="🇺🇸United States - EN"
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link style={{ borderRight: "1px solid #D3D3D3" }} href="#link">
+              Join Now
+            </Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const navbar = () => {
+    return(
+        <h1>navbar</h1>
+    )
+    
+}
+export default navbar
